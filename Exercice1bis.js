3794be8866495f633cc766e9c7482b1ee15b0310
cyril <jesuisledelegue@gmail.com>
2021-10-13 21:42:56 +0200

    commenting all Exercice1bis.js

--- Exercice1bis.js
+++ Exercice1bis.js
@@ -1,3 +1,12 @@
+/**
+ * Fonction doTheCalcul, qui prends comme paramètres l'operateur ainsi que les nombres qui doivent
+ * être calculés. Le nombre d'argument est ilimité sauf pour la division (ça n'a pas de sens sinon, fin
+ * peut être mais j'ai pas réussi à l'implémenter)
+ * La fonction renvoie la valeur des operations
+ * @param operateur '+', '-', '*', '/', '^'
+ * @param args Les nombres
+ * @returns {number} La valeur du calcul
+ */
 const doTheCalcul = (operateur, ...args) => {
     let resultat;
     switch (operateur) {
@@ -29,6 +38,12 @@ const doTheCalcul = (operateur, ...args) => {
     return resultat;
 }
 
+/**
+ * La Fonction calculerLesParentheses, prend en entrée une string ou il y a des parenthéses. Elle extrait les parenthéses
+ * et les calule. Elle remplace ensuite ces parenthéses par leur valeur dans le string : "(2+4)-6" => "6-6"
+ * @param calcul string du type "(a/b)*c^d+(e-f)"
+ * @returns {string} string du type "A*c^d+E"
+ */
 const calculerLesParentheses = (calcul) => {
 
     while (calcul.indexOf('(') !== -1){
@@ -76,32 +91,44 @@ const calculerLesParentheses = (calcul) => {
 
     return calcul;
 }
-
-const doAllCalcul = (opperator, calcul) => {
-    while (calcul.indexOf(opperator) !== -1){
+/**
+ * Fonction doAllCalcul est la fonction qui effectue tous les calculs du type operator qu'il trouve dans le string calcul.
+ * Il retourne le string avec les valeurs calculées à la place des calculs : "3+2" => "5"
+ * @param operator '+', '-', '*', '/', '^'
+ * @param calcul string du type "a/b*c^d+e-f"
+ * @returns {string} string du type "a/b*c^d+e-f"
+ */
+const doAllCalcul = (operator, calcul) => {
+    while (calcul.indexOf(operator) !== -1){
         let nbDroit, nbGauche, start, end;
-        let i = calcul.indexOf(opperator) - 1;
+        let i = calcul.indexOf(operator) - 1;
 
         while(parseInt(calcul[i]) == calcul[i] || calcul[i] === '.'){
             i--;
         }
-        nbGauche = parseFloat(calcul.slice(i + 1, calcul.indexOf(opperator)));
+        nbGauche = parseFloat(calcul.slice(i + 1, calcul.indexOf(operator)));
         start = i + 1;
-        i = calcul.indexOf(opperator) + 1;
+        i = calcul.indexOf(operator) + 1;
 
         while(parseFloat(calcul[i]) == calcul[i] || calcul[i] === '.'){
             i++;
         }
-        nbDroit = parseFloat(calcul.slice(calcul.indexOf(opperator) + 1, i));
+        nbDroit = parseFloat(calcul.slice(calcul.indexOf(operator) + 1, i));
         end = i;
 
-        calcul = calcul.slice(0, start) + doTheCalcul(opperator, nbGauche, nbDroit) + calcul.slice(end, calcul.length);
+        calcul = calcul.slice(0, start) + doTheCalcul(operator, nbGauche, nbDroit) + calcul.slice(end, calcul.length);
     }
 
     return calcul;
 }
 
-const opperation = (calcul) =>{
+/**
+ * Fonction operation est la fonction qui fait le pont entre toutes les fonctions précédentes. Elle reçoit en entrée un
+ * calcul sous la forme d'un string et en resort le résultat.
+ * @param calcul string du type "a/b*c^d+e-f"
+ * @returns {float} resultat du calcul
+ */
+const operation = (calcul) =>{
     let retour;
 
     if (calcul.length < 3){ // S'il n'y a pas assez d'éléments pour faire le calcul
@@ -120,8 +147,8 @@ const opperation = (calcul) =>{
 
     }
 
-    return calcul;
+    return parseFloat(calcul);
 };
 
-console.log(opperation("(4+5)^2*(99/100)^2"));
-console.log(opperation("2^(4+5)-2*1.66"));
+console.log(operation("(4+5)^2*(99/100)^2"));
+console.log(operation("2^(4+5)-2*1.66"));
